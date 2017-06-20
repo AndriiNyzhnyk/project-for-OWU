5a6e292525f587a69032e09224973c8da6f59a6d
Andrii Nyzhnyk <andrij1510@gmail.com>
2017-06-20 17:47:33 +0300

    add sliderForCompany

---
+++ script/slider.js
@@ -0,0 +1,23 @@
+var container = document.getElementById("container");
+var slider = document.getElementsByClassName("slider");
+var sliderWidth = slider[0].offsetWidth;
+var sliderLength = slider.length;
+container.style.width = sliderWidth * sliderLength + sliderLength * 4 + "px";
+
+var trust = 0;
+var shift = 0;
+var i = 1;
+var interval = setInterval(function () {
+    slider[i].style.marginLeft = -sliderWidth - 4 + shift + "px";
+    if(trust >= 0) { i++; } else { i--; }
+    if(i > sliderLength - 1) {
+        trust--;
+        shift = sliderWidth + sliderWidth;
+        i--;
+    }
+    if(i <= 0) {
+        trust++;
+        i = 1;
+        shift = 0;
+    }
+}, 2500);
