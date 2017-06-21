c7f75ec342bf0c5f0a1617be79a8c27f32157ef9
Andrii Nyzhnyk <andrij1510@gmail.com>
2017-06-21 16:46:32 +0300

    sliderUpdate V-1

--- script/main.js
+++ script/main.js
@@ -1,32 +1,35 @@
+window.onload = function () {
 // Button Run in index.html
-function showText(el) {
-    if(el.previousElementSibling.clientHeight === 185) {
-        el.previousElementSibling.style.height = "100%";
-        el.innerHTML = "Сховати текст";
-    } else {
-        el.previousElementSibling.style.height = "185px";
-        el.innerHTML = "Читати далі ...";
+    function showText(el) {
+        if (el.previousElementSibling.clientHeight === 185) {
+            el.previousElementSibling.style.height = "100%";
+            el.innerHTML = "Сховати текст";
+        } else {
+            el.previousElementSibling.style.height = "185px";
+            el.innerHTML = "Читати далі ...";
+        }
     }
-}
 
 // Google Map
-var map;
-function initMap() {
-    navigator.geolocation.getCurrentPosition(function (pos) {
-        var myLatLng = {lat: pos.coords.latitude, lng: pos.coords.longitude};
+    var map;
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            zoom: 15,
-            backgroundColor: "silver"
+    function initMap() {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            var myLatLng = {lat: pos.coords.latitude, lng: pos.coords.longitude};
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: myLatLng,
+                zoom: 15,
+                backgroundColor: "silver"
+            });
+
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                title: 'Hello World!',
+                scrollwheel: false
+            });
+
         });
-
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!',
-            scrollwheel: false
-        });
-
-    });
+    }
 }
--- script/slider.js
+++ script/slider.js
@@ -1,23 +1,38 @@
-var container = document.getElementById("container");
-var slider = document.getElementsByClassName("slider");
-var sliderWidth = slider[0].offsetWidth;
-var sliderLength = slider.length;
-container.style.width = sliderWidth * sliderLength + sliderLength * 4 + "px";
+window.onload = function () {
 
-var trust = 0;
-var shift = 0;
-var i = 1;
-var interval = setInterval(function () {
-    slider[i].style.marginLeft = -sliderWidth - 4 + shift + "px";
-    if(trust >= 0) { i++; } else { i--; }
-    if(i > sliderLength - 1) {
-        trust--;
-        shift = sliderWidth + sliderWidth;
-        i--;
+    var textOurProject = document.getElementById("TextOurProject").offsetWidth;
+    console.log(textOurProject);
+    var slr = document.getElementsByClassName("slr");
+    for(var i = 0; i < slr.length; i++) {
+        slr[i].style.width = textOurProject + "px";
     }
-    if(i <= 0) {
-        trust++;
-        i = 1;
-        shift = 0;
-    }
-}, 2500);
+
+
+    var container = document.getElementById("container");
+    var slider = document.getElementsByClassName("slider");
+    var sliderWidth = slider[0].offsetWidth;
+    var sliderLength = slider.length;
+    container.style.width = sliderWidth * sliderLength + sliderLength * 4 + "px";
+
+    var trust = 0;
+    var shift = 0;
+    var i = 1;
+    var interval = setInterval(function () {
+        slider[i].style.marginLeft = -sliderWidth - 4 + shift + "px";
+        if (trust >= 0) {
+            i++;
+        } else {
+            i--;
+        }
+        if (i > sliderLength - 1) {
+            trust--;
+            shift = sliderWidth + sliderWidth;
+            i--;
+        }
+        if (i <= 0) {
+            trust++;
+            i = 1;
+            shift = 0;
+        }
+    }, 2500);
+}
